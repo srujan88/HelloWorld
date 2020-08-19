ddedb117596b34e7c9e2479bc5801c6b7c9bb124
asksr <asksrujank@gmail.com>
2020-08-19 13:37:29 -0500

    Srujan-First_Commit

---
+++ Polymorphism/src/pol/MainClass.java
@@ -0,0 +1,14 @@
+package pol;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+	
+					MyClass t = new MyClass(0);
+					t.info();
+					t.info("overloaded method");
+					new MyClass();
+
+	}
+
+}
---
+++ Polymorphism/src/pol/MyClass.java
@@ -0,0 +1,28 @@
+package pol;
+
+class MyClass {
+	int height;
+	
+	MyClass(){
+		System.out.println("bricks");
+		
+	}
+MyClass(int i){
+	System.out.println("Building new House that is  " + i + " feet tall ");
+	height = i;
+}
+ void info(){
+	 System.out.println("House is " + height + " feet tall ");
+	 
+	}
+ 
+ void info(String s){ 
+ System.out.println(s + " House is " + height + " feet tall");
+ }
+
+
+
+
+	}
+
+
