2ab6a94f6114e31dd65da2ad268d4bbc4b755e70
asksr <asksrujank@gmail.com>
2020-08-19 14:26:24 -0500

    Constructor-Commit

---
+++ Constructors/src/org/JavaPractice/Demo.java
@@ -0,0 +1,14 @@
+package org.JavaPractice;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		Human human = new Human();
+		
+		System.out.println(human.fit());
+		
+			
+
+	}
+
+}
---
+++ Constructors/src/org/JavaPractice/Human.java
@@ -0,0 +1,50 @@
+
+package org.JavaPractice;
+
+public class Human {
+ 
+ private String name="srujan";
+ private String address="SRNAGAR";
+ private int age=30;
+ 
+ public String getName() {
+	return name;
+}
+
+public void setName(String name) {
+	this.name = name;
+}
+
+public String getAddress() {
+	return address;
+}
+
+public void setAddress(String address) {
+	this.address = address;
+}
+
+public int getAge() {
+	return age;
+}
+
+public void setAge(int age) {
+	this.age = age;
+}
+
+public String fit()
+{
+	if (name.equals("srujan") && address.equals("SRNAGAR") && age>=30) {
+		return "perfect";
+	}else{
+			return "not perfect";
+		}
+}
+
+	
+	
+
+}
+ 
+ 
+ 
+
